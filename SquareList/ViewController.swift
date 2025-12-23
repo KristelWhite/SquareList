fbecb7c230cedeece187988fe94195c2901c2cb6
Kristina Pastukhova <k.pastukhova@andersenlab.com>
2025-12-23 16:00:33 +0500

    Initial Commit

---
+++ SquareList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SquareList
+//
+//  Created by Кристина on 23.12.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SquareListUITests/SquareListUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  SquareListUITestsLaunchTests.swift
+//  SquareListUITests
+//
+//  Created by Кристина on 23.12.2025.
+//
+
+import XCTest
+
+final class SquareListUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
